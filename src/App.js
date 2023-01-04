b7146c22c95f7d022ce102ce9b6d9eb04b7c6935
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-01-04 20:15:19 +0100

    Personalizando los iconos del menu

--- src/App.js
+++ src/App.js
@@ -34,12 +34,15 @@ import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './components/react-admin/users';
 import { PostList, PostEdit, PostCreate } from './components/react-admin/posts';
 
+import PostIcon from '@mui/icons-material/Book';
+import UserIcon from '@mui/icons-material/Group';
+
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-    <Resource name="users" list={UserList} recordRepresentation="name" />
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name" />
   </Admin>
 );
 
