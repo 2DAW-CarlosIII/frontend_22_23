fbe88214a442465119b95ea6f4d3bfb731cff27c
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-01-04 19:15:08 +0100

    Definiendo la lista de columnas de la tabla con un List

--- src/App.js
+++ src/App.js
@@ -29,14 +29,15 @@ function App() {
 export default App;
 */
 
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './components/users';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
+    <Resource name="users" list={UserList} />
   </Admin>
 );
 
